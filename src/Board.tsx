bfa89259f486ae54a7e157f85b1416f753d6c939
najongjine <najongjin3@hotmail.com>
2026-01-14 13:26:03 +0900

    .

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 function Board() {
+  const [myinput, setMyinput] = useState("");
+  const [boardList, setBoardList] = useState<string[]>([]);
   return (
     <div>
       <h1>Board</h1>
+      <div>
+        <label>글 입력:</label>
+        <input
+          value={myinput}
+          onChange={(e) => {
+            setMyinput(e.target.value);
+          }}
+        />
+      </div>
     </div>
   );
 }
