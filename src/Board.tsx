4e0b864419b3f647203e75cd98038712f7f5a779
najongjine <najongjin3@hotmail.com>
2026-01-14 13:35:55 +0900

    .

--- src/Board.tsx
+++ src/Board.tsx
@@ -7,9 +7,24 @@ function Board() {
     <div>
       <h1>Board</h1>
       <div>
+        {/* 인자는 ( )로 감싸고, 리턴 부분도 ( )로 감싸세요 */}
+        {boardList?.map((item, index) => (
+          <div key={index}>
+            <div>{item}</div>
+          </div>
+        ))}
+      </div>
+      <div>
         <label>글 입력:</label>
         <input
           value={myinput}
+          onKeyDown={(e) => {
+            if (e.code.toLowerCase().includes("enter")) {
+              let _temp = [...boardList, myinput];
+              console.log(_temp);
+              setBoardList(_temp);
+            }
+          }}
           onChange={(e) => {
             setMyinput(e.target.value);
           }}
