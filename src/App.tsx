3c81625345a0ce3329b965ed69cac647e259aee5
najongjine <najongjin3@hotmail.com>
2026-01-19 15:47:57 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Header from "./Component/Header";
 import Footer from "./Component/Footer";
 import Calc from "./Calc";
 import Board from "./Board";
+import Register from "./Register";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Calc />} />
           <Route path="/board" element={<Board />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
         <Footer />
       </BrowserRouter>
