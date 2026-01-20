0eea9706fe6e6edb710c1753834c590ab0b5dc5c
najongjine <najongjin3@hotmail.com>
2026-01-20 14:06:16 +0900

    .

---
+++ src/Login.tsx
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = async () => {
+    if (!username || !password) {
+      alert("Please enter both username and password.");
+      return;
+    }
+
+    try {
+      const formData = new FormData();
+      formData.append("username", username);
+      formData.append("password", password);
+
+      const response = await fetch(`${API_BASE_URL}/api/user/login`, {
+        method: "POST",
+        body: formData,
+      });
+
+      const data = await response.json();
+      console.log(data);
+
+      if (data.success) {
+        alert("Registration successful! \nUser ID: " + data.data.userInfo.id);
+        // Optional: Redirect or clear form here
+        setUsername("");
+        setPassword("");
+        navigate("/");
+      } else {
+        alert("Registration failed: " + (data.msg || "Unknown error"));
+      }
+    } catch (error) {
+      console.error("Error registering:", error);
+      alert("An error occurred during registration.");
+    }
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleRegister();
+    }
+  };
+
+  return (
+    <div>
+      <h1>Register</h1>
+
+      <div>
+        <label>username:</label>
+        <input
+          value={username}
+          onKeyDown={handleKeyDown}
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+      </div>
+      <div>
+        <label>password:</label>
+        <input
+          type="password"
+          value={password}
+          onKeyDown={handleKeyDown}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+      </div>
+      <div>
+        <button onClick={handleRegister}>Register</button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
