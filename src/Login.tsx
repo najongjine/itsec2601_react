e63ac2afcf46fa5998a0a189172d724e9f1595d0
najongjine <najongjin3@hotmail.com>
2026-01-20 14:13:37 +0900

    .

--- src/Login.tsx
+++ src/Login.tsx
@@ -7,6 +7,13 @@ function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
+  /** 앱은, 화면끼리 데이터 공유를 못해요.
+   * login 페이지에서 token을 받고, Calc 같은 페이지로 이동하면,
+   * 서버에서 받았던 정보 싹 날라가요
+   * 이걸 고치려면 페이지끼리 데이터를 공유할 수 있는 방법을 찾아야 해요.
+   1. query string : 주소창에 데이터 붙이는건데, 이건 정보보안법에 걸려요
+   2. Context API : 이건 공용 저장소를 쉽게 안보이는곳에 저장해서 공유하는 방법이에요
+   */
   const handleRegister = async () => {
     if (!username || !password) {
       alert("Please enter both username and password.");
@@ -27,17 +34,17 @@ function Login() {
       console.log(data);
 
       if (data.success) {
-        alert("Registration successful! \nUser ID: " + data.data.userInfo.id);
+        alert("login successful! \nUser ID: " + data.data.userInfo.id);
         // Optional: Redirect or clear form here
         setUsername("");
         setPassword("");
         navigate("/");
       } else {
-        alert("Registration failed: " + (data.msg || "Unknown error"));
+        alert("login failed: " + (data.msg || "Unknown error"));
       }
     } catch (error) {
-      console.error("Error registering:", error);
-      alert("An error occurred during registration.");
+      console.error("Error login:", error);
+      alert("An error occurred during login.");
     }
   };
 
@@ -49,7 +56,7 @@ function Login() {
 
   return (
     <div>
-      <h1>Register</h1>
+      <h1>Login</h1>
 
       <div>
         <label>username:</label>
@@ -73,7 +80,7 @@ function Login() {
         />
       </div>
       <div>
-        <button onClick={handleRegister}>Register</button>
+        <button onClick={handleRegister}>Login</button>
       </div>
     </div>
   );
