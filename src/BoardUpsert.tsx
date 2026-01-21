d519f2598e14a77b45d0fb87382436a23e6f0d57
najongjine <najongjin3@hotmail.com>
2026-01-21 13:38:30 +0900

    .

--- src/BoardUpsert.tsx
+++ src/BoardUpsert.tsx
@@ -5,9 +5,9 @@ import { useAuth } from "./context/AuthContext";
 function BoardUpsert() {
   const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
   const navigate = useNavigate();
-  const { login } = useAuth();
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const { userInfo, token } = useAuth();
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
 
   /** 앱은, 화면끼리 데이터 공유를 못해요.
    * login 페이지에서 token을 받고, Calc 같은 페이지로 이동하면,
@@ -16,18 +16,18 @@ function BoardUpsert() {
    1. query string : 주소창에 데이터 붙이는건데, 이건 정보보안법에 걸려요
    2. Context API : 이건 공용 저장소를 쉽게 안보이는곳에 저장해서 공유하는 방법이에요
    */
-  const handleRegister = async () => {
-    if (!username || !password) {
-      alert("Please enter both username and password.");
+  const onUpdate = async () => {
+    if (!title || !content) {
+      alert("제목과 내용을 입력해주세요.");
       return;
     }
 
     try {
       const formData = new FormData();
-      formData.append("username", username);
-      formData.append("password", password);
+      formData.append("title", title);
+      formData.append("content", content);
 
-      const response = await fetch(`${API_BASE_URL}/api/user/login`, {
+      const response = await fetch(`${API_BASE_URL}/api/board/upsert`, {
         method: "POST",
         body: formData,
       });
@@ -39,25 +39,19 @@ function BoardUpsert() {
         data = data?.data;
         // alert("login successful! \nUser ID: " + data.userInfo.id);
 
-        // Store in context
-        login(data?.userInfo, data?.token);
-
-        // Optional: Redirect or clear form here
-        setUsername("");
-        setPassword("");
-        navigate("/");
+        navigate("/board");
       } else {
-        alert("login failed: " + (data.msg || "Unknown error"));
+        alert("글쓰기 실패: " + (data.msg || "Unknown error"));
       }
     } catch (error) {
       console.error("Error login:", error);
-      alert("An error occurred during login.");
+      alert("글쓰기 실패: An error occurred during login.");
     }
   };
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter") {
-      handleRegister();
+      onUpdate();
     }
   };
 
