6ce38dd26653af275cf65798cb278fd8e298f011
najongjine <najongjin3@hotmail.com>
2026-01-26 15:09:28 +0900

    .

--- src/BoardDetail.tsx
+++ src/BoardDetail.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import { useAuth } from "./context/AuthContext";
 import { MemoStrtype } from "./types/global_types";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import DOMPurify from "dompurify";
 
 function BoardDetail() {
   const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+  const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
   const boardId = Number(searchParams.get(`id`));
   const { userInfo, token } = useAuth();
@@ -51,12 +52,54 @@ function BoardDetail() {
       <h1>Board 상세</h1>
 
       <div>{board?.title}</div>
+      <div>{board?.createdDt}</div>
+      <div>{board?.username ?? "몰라"}</div>
       <div
         className="view-content"
         dangerouslySetInnerHTML={{
           __html: DOMPurify.sanitize(board?.htmlContent || ""),
         }}
       />
+
+      <div>
+        <button
+          title="삭제"
+          onClick={async () => {
+            try {
+              if (!confirm(`정말로 삭제 하시겠습니까?`)) {
+                return;
+              }
+              const payload = {
+                id: boardId,
+              };
+              let res: any = await fetch(
+                `${API_BASE_URL}/api/board_v2/delete_by_id`,
+                {
+                  method: "POST",
+                  headers: {
+                    Authorization: `${token}`,
+                  },
+                  body: JSON.stringify(payload),
+                },
+              );
+
+              res = await res.json();
+              console.log("--- res: ", res);
+              if (!res?.success) {
+                alert(`저장 실패. ${res?.msg ?? ""}`);
+                return;
+              }
+
+              navigate("/board");
+            } catch (error: any) {
+              alert(`문서 저장 중 오류 발생. ${error?.msg ?? ""}`);
+              // 사용자에게 오류를 알리는 로직 추가
+            }
+          }}
+        >
+          삭제
+        </button>
+      </div>
     </div>
   );
 }
